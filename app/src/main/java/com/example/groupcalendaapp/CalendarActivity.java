5fdf0b9766c771b3640fc7bcc543cca131d2356a
Adam Gerrish <agerrish77@hotmail.com>
2022-12-05 13:41:47 -0500

    added help menu and toolbar

--- app/src/main/java/com/example/groupcalendaapp/CalendarActivity.java
+++ app/src/main/java/com/example/groupcalendaapp/CalendarActivity.java
@@ -2,14 +2,20 @@ package com.example.groupcalendaapp;
 
 import static com.example.groupcalendaapp.CalUtils.monthYearFromDate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -27,8 +33,40 @@ public class CalendarActivity extends AppCompatActivity implements CalendarAdapt
         initWidgets();
         CalUtils.selectedDate = LocalDate.now();
         setMonthView();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Welcome to HUSTLE! Click on the 3 dots for the Help menu and more", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this,"Help Menu", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(CalendarActivity.this,HelpActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.item3:
+                Toast.makeText(this,"KNOW MORE", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem1:
+                Toast.makeText(this,"Set Event", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem2:
+                Toast.makeText(this,"Week View", Toast.LENGTH_SHORT).show();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+
+    }
 
     private void initWidgets() {
         calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
--- app/src/main/java/com/example/groupcalendaapp/WeekView.java
+++ app/src/main/java/com/example/groupcalendaapp/WeekView.java
@@ -2,13 +2,18 @@ package com.example.groupcalendaapp;
 
 import static com.example.groupcalendaapp.CalUtils.monthYearFromDate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -31,14 +36,42 @@ public class WeekView extends AppCompatActivity implements CalendarAdapter.OnIte
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         setContentView(R.layout.activity_week_view);
         initWidgets();
         loadDbToMemory();
         setWeekView();
         clickableList();
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Welcome to HUSTLE! Click on the 3 dots for the Help menu and more", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this,"Help Menu", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this,"KNOW MORE", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem1:
+                Toast.makeText(this,"Set Event", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem2:
+                Toast.makeText(this,"Week View", Toast.LENGTH_SHORT).show();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+
+    }
     private void initWidgets() {
         calRecyclerView = findViewById(R.id.calendarRecyclerView);
         monthYrTxt = findViewById(R.id.monthYearTV);
