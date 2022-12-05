7cc4413817e29ad2e3fa3dac491902f1ad620703
Sakshiiiis <116122072+sakshiiiis@users.noreply.github.com>
2022-12-05 12:49:15 -0500

    displays 1 additional quote

--- app/src/main/java/com/example/groupcalendaapp/QuoteActivity.java
+++ app/src/main/java/com/example/groupcalendaapp/QuoteActivity.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class QuoteActivity extends AppCompatActivity {
-    ArrayList<String> listQuotes = new ArrayList<String>();
+    ArrayList<String> listQuotes;
     int quoteNumber;
     String mainText;
     TextView quoteTV;
@@ -22,21 +22,9 @@ public class QuoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote);
-        listQuotes.add(String.valueOf(R.string.quote1));
-        listQuotes.add(String.valueOf(R.string.quote2));
-        listQuotes.add(String.valueOf(R.string.quote3));
-        listQuotes.add(String.valueOf(R.string.quote4));
-        listQuotes.add(String.valueOf(R.string.quote5));
-        listQuotes.add(String.valueOf(R.string.quote6));
-        listQuotes.add(String.valueOf(R.string.quote7));
-        listQuotes.add(String.valueOf(R.string.quote8));
-        listQuotes.add(String.valueOf(R.string.quote9));
-        listQuotes.add(String.valueOf(R.string.quote10));
 
-        quoteNumber=0;
+        listQuotes = quotes();
 
-        //quoteOnAppLoaded();
-        //clickNewQuote();
 
         quoteTV = findViewById(R.id.quoteTV);
         quoteButton = findViewById(R.id.fabNewQuote);
@@ -52,10 +40,23 @@ public class QuoteActivity extends AppCompatActivity {
 
     }
 
+    public ArrayList<String> quotes(){
+        ArrayList<String> listQuotes = new ArrayList<String>();
+       listQuotes.add("Do not fear failure but rather fear not trying.");
+       listQuotes.add("Without ambition one starts nothing. Without work one finishes nothing. The prize will not be sent to you. You have to win it.");
+       listQuotes.add("Action may not always bring happiness, but there is no happiness without action.");
+       listQuotes.add("“Don't fear failure. — Not failure, but low aim, is the crime. In great attempts it is glorious even to fail.");
+       listQuotes.add("First say to yourself what you would be; and then do what you have to do.");
+       listQuotes.add("Everything is hard before it is easy");
+       listQuotes.add("Make today worth remembering.");
+
+
+        return listQuotes;
+    }
+
     public void printQuote(){
         Random quoteNumber = new Random();
-        String quoteString = listQuotes.get(quoteNumber.nextInt() % listQuotes.size());
-        quoteTV.setText(quoteString);
-
+        String quoteString = listQuotes.get(quoteNumber.nextInt() % listQuotes.size()).toString();
+        quoteTV.setText(listQuotes.get(2));
     }
 }
